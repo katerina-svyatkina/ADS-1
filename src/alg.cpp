a84c4c80cc7c0fa1c7dabc85466a6b1328c4a9ff
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-02-14 20:47:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,42 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value < 2) return false;
+	if (value == 2) return true;
+	for (uint64_t i = 2; i <= sqrt(value); i++) {
+		if (!(value % i)) {
+			return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t  temp_num=0;
+	for (uint64_t i = 2; i < UINT64_MAX; i++) {
+		if (checkPrime(i)) {
+			temp_num++;
+		}
+		if (n == temp_num) {
+			return i;
+		}
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+for (uint64_t i = value+1; i < UINT64_MAX; i++) {
+		if (checkPrime(i)) {
+			return i;
+		}	
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sum = 0;
+	for (int i = 2; i < hbound; i++) {
+		if (checkPrime(i)) {
+			sum = sum + i;
+		}
+	}
+	return sum;
 }
