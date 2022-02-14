b4961d1b163c3da6787fb771c34ee771d07bbda2
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-02-14 21:50:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
 
 
@@ -15,7 +16,7 @@ return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-uint64_t  temp_num=0;
+uint64_t  temp_num = 0;
 for (uint64_t i = 2; i < UINT64_MAX; i++) {
 if (checkPrime(i)) {
 temp_num++;
@@ -24,14 +25,16 @@ if (n == temp_num) {
 return i;
 }
 }
+return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
 for (uint64_t i = value+1; i < UINT64_MAX; i++) {
 if (checkPrime(i)) {
 return i;
-}	
 }
+}
+return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
